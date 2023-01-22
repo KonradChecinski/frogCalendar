081add4717011cb966a5165ba79f7adb091779b1
Gromzi <mamgier@gmail.com>
2023-01-22 21:43:10 +0100

    Rozwiązywanie konfliktów

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,12 +1,6 @@
 <script setup lang="ts">
 import CalendarMonth from "@/components/Calendar/CalendarMonth.vue";
 import CalendarWeek from "@/components/Calendar/CalendarWeek.vue";
-import drawerMenu from "@/components/drawerMenu.vue";
-import { usefetch } from "@/stores/fetch";
-import { createPinia } from "pinia";
-
-const pinia = createPinia();
-const getDate = usefetch(pinia);
 </script>
 
 <template>
