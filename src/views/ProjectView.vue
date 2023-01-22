94398aa3b223941fa66b2daed93b38052b0e3377
Gromzi <mamgier@gmail.com>
2023-01-22 21:36:51 +0100

    Widok miesięczny i wysuwane menu zrobione

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import CalendarMonth from '@/components/Calendar/CalendarMonth.vue';
-import CalendarWeek from '@/components/Calendar/CalendarWeek.vue';
-import { usefetch } from '@/stores/fetch';
-import { createPinia } from 'pinia';
+import CalendarMonth from "@/components/Calendar/CalendarMonth.vue";
+import CalendarWeek from "@/components/Calendar/CalendarWeek.vue";
+import drawerMenu from "@/components/drawerMenu.vue";
+import { usefetch } from "@/stores/fetch";
+import { createPinia } from "pinia";
 
 const pinia = createPinia();
-const getDate = usefetch(pinia)
-
+const getDate = usefetch(pinia);
 </script>
 
 <template>
@@ -15,13 +15,8 @@ const getDate = usefetch(pinia)
     <!-- <CalendarWeek>
       Kalendarz2tyg
     </CalendarWeek> -->
+    <drawerMenu />
   </div>
 </template>
 
-<style scoped>
-.app-views-container {
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-}
-</style>
+<style scoped src="@/assets/css/projectView.css"></style>
