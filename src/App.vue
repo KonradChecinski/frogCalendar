ae1a24db58a4054b84356b5aa86b27d64b4e202a
Gromzi <mamgier@gmail.com>
2023-01-26 19:26:18 +0100

    Ukrycie manuDrawer przy logowaniu, tyle że krócej

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,55 @@
 <script setup lang="ts">
-import { routerKey, RouterLink, RouterView, useRouter } from 'vue-router'
-import { useLoggedStore } from './stores/LoggedIn';
+import { routerKey, RouterLink, RouterView, useRouter } from "vue-router";
+import { useLoggedStore } from "./stores/LoggedIn";
 import drawerMenu from "@/components/drawerMenu.vue";
 
-import router from './router';
+import router from "./router";
 
 const LoggedStore = useLoggedStore();
 
-if(isLocalStorageAvailable()){
-    if(localStorage.getItem('APIKey') !== null){
-      LoggedStore.APIKey = localStorage.getItem('APIKey')
-      LoggedStore.email = localStorage.getItem('email')
-      LoggedStore.username = localStorage.getItem('username')
-      LoggedStore.isLoggedIn = true;
-    } 
+if (isLocalStorageAvailable()) {
+  if (localStorage.getItem("APIKey") !== null) {
+    LoggedStore.APIKey = localStorage.getItem("APIKey");
+    LoggedStore.email = localStorage.getItem("email");
+    LoggedStore.username = localStorage.getItem("username");
+    LoggedStore.isLoggedIn = true;
+  }
 }
 
 router.beforeEach((to, from, next) => {
-    if(LoggedStore.isLoggedIn && to.matched.some(record => record.path == '/login')){
-        next('/');
-    }
-    else{
-        if (to.matched.some(record => record.meta.requiresAuth)) {
-            if (!LoggedStore.isLoggedIn) {
-                next({ name: 'Login' })
-            } else {
-                next();
-            }
-        } else {
-            next()
-        }
-    }
-
-})
-
-function isLocalStorageAvailable(){
-    var test = 'test';
-    try {
-        localStorage.setItem(test, test);
-        localStorage.removeItem(test);
-        return true;
-    } catch(e) {
-        return false;
+  if (
+    LoggedStore.isLoggedIn &&
+    to.matched.some((record) => record.path == "/login")
+  ) {
+    next("/");
+  } else {
+    if (to.matched.some((record) => record.meta.requiresAuth)) {
+      if (!LoggedStore.isLoggedIn) {
+        next({ name: "Login" });
+      } else {
+        next();
+      }
+    } else {
+      next();
     }
   }
+});
 
-
+function isLocalStorageAvailable() {
+  var test = "test";
+  try {
+    localStorage.setItem(test, test);
+    localStorage.removeItem(test);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
 </script>
 
 <template>
   <RouterView />
-  <drawerMenu v-if="router.currentRoute.value.path != `/login` && router.currentRoute.value.path != `/register`"/>
+  <drawerMenu v-if="LoggedStore.isLoggedIn" />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
